35f1848b6726f882cba735bbb87305742256caa4
RJalbers <RJalbers89@gmail.com>
2019-12-25 06:30:41 -0800

    finished game

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -10,10 +10,9 @@ GAME RULES:
 */
 
 var scores, roundScore, activePlayer;
+var gamePLaying = true; 
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+init();
 //command to select the dice class and edit CSS attributes
 //we are setting it not to show anything until the event handler.
 document.querySelector('.dice').style.display = 'none';
@@ -30,39 +29,44 @@ document.getElementById('current-1').textContent = '0';
 // in this case it's when the we click on the btn-roll
 document.querySelector('.btn-roll').addEventListener('click', function () {
 
-    //this is how you can set up a random number generator using
-    //the math function
+        if(gamePLaying) {
+        //this is how you can set up a random number generator using
+        //the math function
 
-    //1. Random number
-    var dice = Math.floor(Math.random() * 6) + 1;
+        //1. Random number
+        var dice = Math.floor(Math.random() * 6) + 1;
 
-    //we can set up a varaible to shorten the process of selecting
-    // classes and such. Always look for oppurtunities to simplify code.
+        //we can set up a varaible to shorten the process of selecting
+        // classes and such. Always look for oppurtunities to simplify code.
 
-    //2. Display the result
-    var diceDOM = document.querySelector('.dice')
+        //2. Display the result
+        var diceDOM = document.querySelector('.dice');
 
-    //when the eventHandler is triggered we are making the dice appear
-    //on the screen and also linking pics with the random number generated.
-    //we are doing this by having the dice random funtion add on to the strings to select a picture in the files
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
+        //when the eventHandler is triggered we are making the dice appear
+        //on the screen and also linking pics with the random number generated.
+        //we are doing this by having the dice random funtion add on to the strings to select a picture in the files
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
 
 
-    //3. Update the round score IF the rolled number was Not a 1
-    if (dice !== 1) {
-        //add score
-        roundScore += dice;
-        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        //3. Update the round score IF the rolled number was Not a 1
+        if (dice !== 1) {
+            //add score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
 
-    } else {
+        } else {
 
-        roundScore = 0;
-        //next player
-        nextPlayer()
-    }
+            roundScore = 0;
+            //next player
+            nextPlayer()
+        }
+        }
     });
 
+
+
+if (gamePLaying) {
     document.querySelector('.btn-hold').addEventListener('click', function () {
         //Add Current score to GLOBAL score
         scores[activePlayer] += roundScore;
@@ -72,40 +76,69 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
         //update the UI 
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-       
 
-        
+
+
         //check if the player won the game
 
         if (scores[activePlayer] >= 100) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-            document.querySelector('.dice').style. display = 'none';
+            document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active')
+            gamePLaying = false;
         } else {
             nextPlayer();
         }
-    
+
 
 
 
     });
+}
+function nextPlayer() {
 
-    function nextPlayer() {
-        
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-        
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-    }
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+}
+
+//New game button
+document.querySelector('.btn-new').addEventListener('click', init);
 
 
 
 
-//dice = Math.floor(Math.random() * 6) + 1;
+
+function init() {
+
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
+    gamePLaying = true;
+
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('#name-0').textContent = 'player 1';
+    document.querySelector('#name-1').textContent = 'player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner')
+    document.querySelector('.player-1-panel').classList.remove('winner')
+
+    document.querySelector('.player-0-panel').classList.remove('active')
+    document.querySelector('.player-1-panel').classList.remove('active')
+    document.querySelector('.player-0-panel').classList.add('active')
+
+}
+
+
+    //dice = Math.floor(Math.random() * 6) + 1;
 
 
 //document.querySelector('#current-' + activePlayer).textContent = dice;
